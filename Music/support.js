5712ba530268ed925e81b3e6ebd26c395930bb4a
JANO <77622056+Devjano@users.noreply.github.com>
2021-10-24 15:25:18 -0700

    Create support.js

---
+++ Music/support.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "support",
+  cooldown: 5,
+  description: "Display all commands and descriptions",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setDescription(` 
+\`link server\`
+**[ support ](https://discord.gg/2ugMEJubkt)**   
+ `)
+    
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
